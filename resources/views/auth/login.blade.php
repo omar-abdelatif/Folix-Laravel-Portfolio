f1c72199ba7996c71b5dff6424ec63369ed68af8
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-04-18 00:38:05 +0200

    initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.guest')
+
+@section('content')
+    <div class="card-body login-card-body">
+        <div class="card-image text-center">
+            <img src="{{asset('images/logo.jpg')}}" class="rounded mt-3 mb-3" width="130" alt="">
+        </div>
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+
+            <div class="input-group mb-3">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="{{ __('Email') }}" required autofocus>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+                @error('email')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
+            </div>
+
+            <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('Password') }}" required>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+                @error('password')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
+            </div>
+
+            <div class="row">
+                <div class="col-8">
+                    <div class="icheck-primary">
+                        <input type="checkbox" id="remember" name="remember">
+                        <label for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-4">
+                    <button type="submit" class="btn btn-primary btn-block">{{ __('Login') }}</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
+
+        @if (Route::has('password.request'))
+            <p class="mb-1 d-flex justify-content-between mt-3">
+                <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                <a href="{{ route('register') }}">{{ __('Create New Account') }}</a>
+            </p>
+        @endif
+    </div>
+    <!-- /.login-card-body -->
+@endsection
